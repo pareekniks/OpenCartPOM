6e6fa9a757a8ef831ce83a7a04ff3704baecf070
Nikhil Pareek <niks@192.168.1.2>
2022-10-12 11:36:59 +0530

    docker config and remote implemenation

--- src/main/java/com/qa/opencart/factory/DriverFactory.java
+++ src/main/java/com/qa/opencart/factory/DriverFactory.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
@@ -11,6 +13,9 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.safari.SafariDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -37,18 +42,55 @@ public class DriverFactory {
 
 		if (browserName.equalsIgnoreCase("chrome")) {
 			WebDriverManager.chromedriver().setup();
-
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver("chrome");
+			} else {
+				tlDriver.set(new ChromeDriver(ops.getChromeOptions()));
+			}
 			// driver = new ChromeDriver(ops.getChromeOptions());
-			tlDriver.set(new ChromeDriver(ops.getChromeOptions()));
+			
+		} else if (browserName.equalsIgnoreCase("ff")) {
+			WebDriverManager.firefoxdriver().setup();
+			if (Boolean.parseBoolean(prop.getProperty("remote"))) {
+				init_remoteDriver("ff");
+			} else {
+			tlDriver.set(new FirefoxDriver(ops.getffOptions()));
+			}
+			// driver = new ChromeDriver(ops.getChromeOptions());
+			
 		} else if (browserName.equalsIgnoreCase("safari")) {
 			driver = new SafariDriver();
 		} else {
 			System.out.println("Please pass the correct browser name " + browserName);
 		}
+		
 		getDriver().manage().deleteAllCookies();
 		getDriver().manage().window().maximize();
 		getDriver().get(prop.getProperty("url").trim());
 		return getDriver();
+
+	}
+
+	private void init_remoteDriver(String browser) {
+		System.out.println("Running test cases on remote grid");
+
+		if (browser.equals("chrome")) {
+			try {
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), ops.getChromeOptions()));
+			} catch (MalformedURLException e) {
+				System.out.println("in catch block");
+				e.printStackTrace();
+			}
+		}
+
+		if (browser.equals("ff")) {
+			try {
+				tlDriver.set(new RemoteWebDriver(new URL(prop.getProperty("huburl")), ops.getffOptions()));
+			} catch (MalformedURLException e) {
+				System.out.println("in catch block");
+				e.printStackTrace();
+			}
+		}
 
 	}
 
